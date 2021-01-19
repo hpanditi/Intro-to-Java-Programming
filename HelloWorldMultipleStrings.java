d89cb6e25eeee396fd9ea2dceec695cc990d3516
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-01-19 16:25:34 -0500

    Hello World Multiple Strings

---
+++ HelloWorldMultipleStrings.java
@@ -0,0 +1,6 @@
+public class HelloWorldMultipleStrings {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+        System.out.println(" -- It's a beautiful day on the same line.");
+    } // End main
+} // End HelloWorldMultipleStrings
