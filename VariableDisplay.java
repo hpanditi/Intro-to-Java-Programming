2aa6f3c8fad7f69cdd913cbbd961a4b63d024bb3
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-01-20 20:05:31 -0500

    Hemanth Panditi Variable Display

---
+++ VariableDisplay.java
@@ -0,0 +1,49 @@
+public class VariableDisplay {
+
+    public static void main (String[] args) {
+       int myNumber;
+       
+       myNumber = 77;
+       
+       System.out.print("This is an int: ");
+       System.out.println(myNumber);
+    
+       double myX;
+       
+       myX = 0.0;
+       
+       System.out.print("This is an double: ");
+       System.out.println(myX);
+       
+       double myDouble1, myDouble2;
+       
+       myDouble1 = 1.2345;
+       myDouble2 = 5.4321;
+       
+       System.out.print("This is two double assignments: ");
+       System.out.print(myDouble1);
+       System.out.print(" and ");
+       System.out.println(myDouble2);
+       
+       boolean myBoolean;
+       
+       myBoolean = false;
+       
+       System.out.print("This is a boolean: ");
+       System.out.println(myBoolean);
+       
+       char myChar;
+       
+       myChar = 'A';
+       
+       System.out.print("This is a character assignment: ");
+       System.out.println(myChar);
+       
+       String myString;
+       
+       myString = "string";
+       
+       System.out.print("This is a string assignment: ");
+       System.out.println(myString);
+    }
+}
