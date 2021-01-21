0f7fd577dc2f403977f4b4217184ca6b8d15271a
hpanditi <hpanditi@users.noreply.github.com>
2021-01-20 20:13:29 -0500

    Added changes

--- LiteralDisplay.java
+++ LiteralDisplay.java
@@ -2,41 +2,41 @@
 /**
  * Write a description of class LiteralDisplay here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Hemanth Panditi)
+ * @version (January 20, 2021)
  */
 public class LiteralDisplay
 {
 public static void main(String[] args)
 {
-        // display an int literal
-        System.out.print("This is the integer literal 123: ");
-        System.out.println(123);
+// display an int literal
+System.out.print("This is the integer literal 123: ");
+System.out.println(123);
        
 
-        // display a Floating-Point Number
-        System.out.print("This is a floating point number 1.2: ");
-        System.out.println(1.2);
+// display a Floating-Point Number
+System.out.print("This is a floating point number 1.2: ");
+System.out.println(1.2);
 
-    // display a Character
-    System.out.print("This is a character 'a': ");
-    System.out.println('a');
+// display a Character
+System.out.print("This is a character 'a': ");
+System.out.println('a');
 
-    // display a Boolean
-    System.out.print("This is a boolean true: ");
-    System.out.println(true);
+// display a Boolean
+System.out.print("This is a boolean true: ");
+System.out.println(true);
     
-    // display a binary number
-    System.out.print("This is a binary number 0b0101010: ");
-    System.out.println(0b0101010);
+// display a binary number
+System.out.print("This is a binary number 0b0101010: ");
+System.out.println(0b0101010);
     
-    // display a hexadecimal number
-    System.out.print("This is a hexadecimal number 0x13BD: ");
-    System.out.println(0x13BD);
+// display a hexadecimal number
+System.out.print("This is a hexadecimal number 0x13BD: ");
+System.out.println(0x13BD);
     
-    // display a string
-    System.out.print("This is a string: ");
-    System.out.println("This is a string");
+// display a string
+System.out.print("This is a string: ");
+System.out.println("This is a string");
 }
 }
 
