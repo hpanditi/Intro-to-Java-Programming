ae35df5677e41450b0929f57c677e5164d8ecf20
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-01-30 22:06:24 -0500

    Simple If statements
    Self-created sample practice

---
+++ SampleStatementsIf.java
@@ -0,0 +1,23 @@
+/**
+ * Write a description of class SampleStatementsIf here.
+ *
+ * @author Hemanth Panditi
+ * @version January 30, 2021
+ */
+public class SampleStatementsIf
+{
+    public static void main (String[] args) {
+        
+        int x = 15;
+        if(x < 10) {
+        
+        System.out.println("Yes, it's true! ");        
+}
+        else if(x > 20){
+            System.out.println("No, it's false!");
+}
+        else {
+            System.out.println("None of the above.");
+}
+}
+}
