47bc31210276bffa0360b6f15ee249cd2cd02cdf
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-16 20:50:50 -0500

    Added changes

--- FavoriteFilm.java
+++ FavoriteFilm.java
@@ -1,5 +1,5 @@
 public class FavoriteFilm {
     public static void main(String[] args) {
-        System.out.println ("My favorite movie is The Shawshank Redemption!");
+        System.out.println ("My favorite movie is The Shawshank Redemption! It was directed by Frank Darabont");
     }
 }
