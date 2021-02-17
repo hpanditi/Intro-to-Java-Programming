da08827948b5985f156dbed4d30f4047ff2c02cf
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-02-16 20:52:12 -0500

    Added changes

--- SecondFavoriteFilm.java
+++ SecondFavoriteFilm.java
@@ -1,7 +1,7 @@
 public class SecondFavoriteFilm {
     public static void main(String[] args) {
 
-        System.out.println("My second favorite movie is The Dark Knight!");
+        System.out.println("My second favorite movie is The Dark Knight! It was directed by Christopher Nolan");
 
         }
     }
